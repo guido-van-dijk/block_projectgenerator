bf6f350627c8574c614f09e24919dde470bfead7
guido-vandijk <guido.vandijk@learning-xperience.com>
2025-04-26 08:28:31 +0200

    Update settings.php
    settings changed

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+if ($hassiteconfig) { // Show only to site admins.
+    $settings->add(new admin_setting_configtext(
+        'block_projectgenerator/openai_api_key',
+        get_string('openaiapikey', 'block_projectgenerator'),
+        get_string('openaiapikey_desc', 'block_projectgenerator'),
+        '',
+        PARAM_RAW // Use PARAM_RAW for API keys (allows special chars).
+    ));
