c8108934c6df43cfb40502febf366928523b7c9e
guido-vandijk <guido.vandijk@learning-xperience.com>
2025-04-26 09:12:45 +0200

    Update access.php
    access

---
+++ db/access.php
@@ -0,0 +1,13 @@
+<?php
+$capabilities = array(
+    'block/projectgenerator:addinstance' => array(
+        'riskbitmask' => RISK_NONE,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
