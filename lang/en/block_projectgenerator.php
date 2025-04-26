3b65b50b4b9bef3256511d259f8c4c7fb84ca602
guido-vandijk <guido.vandijk@learning-xperience.com>
2025-04-26 08:46:47 +0200

    Update block_projectgenerator.php
    db added

--- lang/en/block_projectgenerator.php
+++ lang/en/block_projectgenerator.php
@@ -1,7 +1,4 @@
 <?php
 $string['pluginname'] = 'Project Generator';
-$string['enterobjectives'] = 'Enter curriculum objectives (one per line):';
-$string['generateproject'] = 'Generate Project';
-
 $string['openaiapikey'] = 'OpenAI API key';
-$string['openaiapikey_desc'] = 'Enter your OpenAI API key here. This key is used for generating projects via AI. Never share this key with students.';
+$string['openaiapikey_desc'] = 'Enter your OpenAI API key here.';
